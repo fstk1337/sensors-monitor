6b281c333fd8d00ee7e6bff2e41068c3698a53ac
fstk1337 <vit.shvaichuk@gmail.com>
2022-11-19 14:50:42 +0300

    init: Spring Boot server

---
+++ server/src/test/java/com/fstk1337/boot/sensor/SensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstk1337.boot.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
